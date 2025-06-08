5f622bf74f1801c996c3d0d1c32278e43f6e912a
Vnukov Ivan <iavnukov@domclick.ru>
2025-06-09 00:07:42 +0300

    drop tables

---
+++ src/db/migrations/versions/2025_06_09_11850-1a687dfa6884_drop_tables.py
@@ -0,0 +1,42 @@
+"""drop
+
+Revision ID: 1a687dfa6883
+Revises: 2848507dd97d
+Create Date: 2025-05-26 16:07:36.573558
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "1a687dfa6884"
+down_revision: Union[str, None] = "1a687dfa6888"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    op.drop_table("system_settings")
+    op.drop_table("questionaires")
+
+
+def downgrade():
+    op.create_table(
+        "system_settings",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("key", sa.String(length=255), nullable=False),
+        sa.Column("value", sa.String(length=255), nullable=True),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("key"),
+    )
+    op.create_table(
+        "questionaires",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("name", sa.String(length=255), nullable=False),
+        sa.Column("description", sa.Text(), nullable=True),
+        sa.PrimaryKeyConstraint("id"),
+    )
